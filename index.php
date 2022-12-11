a00a0a24146817100a15a1e9fcaf13e978caa813
bsmela1 <bartoszsmela04@gmail.com>
2022-12-11 16:04:15 +0100

    added few files main site

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_main_site.css">
+    <link rel="icon" href="site_icon.jpg">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <title>Kolekcjoner</title>
+</head>
+<body>  
+
+<nav>
+    <a href="https://ih1.redbubble.net/image.1533663591.5353/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.jpg">
+        <img id="site-icon" src="unabomber.jpg"  width="100px" height="100px">
+    </a>
+    <ul id="menu">
+        <li>
+            <a href="kolekcje.php"><h2>kolekcje.</h2></a>
+        </li>
+        <li>
+            <h2>wstaw nowy item.</h2>
+        </li>
+    </ul>
+    <a id="login-button" href="login.php">
+        <img src="logout_icon.jpg" alt="logout" width="55px" height="55px">
+    </a>
+</nav>
+
+<main>
+    <section id="cats"></section>
+</main>
+
+</body>
+</html>
